1e9595cb3f5c1b38b8c1f1db36c06b00e3771353
enriquevaiii <valerioart99@gmail.com>
2019-02-13 09:09:37 -0400

    nuevo repo

---
+++ rockets/keppler_frontend/app/controllers/keppler_frontend/app/frontend_controller.rb
@@ -0,0 +1,22 @@
+require_dependency "keppler_frontend/application_controller"
+
+module KepplerFrontend
+  class App::FrontendController < App::AppController
+    # Begin callbacks area (don't delete)
+    # End callbacks area (don't delete)
+    include FrontsHelper
+    layout 'layouts/keppler_frontend/app/layouts/application'
+
+    # begin keppler
+    def index
+    end
+    # end keppler
+
+    private
+    # begin callback user_authenticate
+    def user_authenticate
+      redirect_to '/' unless user_signed_in?
+    end
+    # end callback user_authenticate
+  end
+end

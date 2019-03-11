56736c750e13d373edb8ddd0b8014b3f90861c1e
maria <castrogonzalezmariajose6@gmail.com>
2019-03-11 14:48:17 -0400

    Validacion de n de url y dimensiones de imagen en publicidad

--- rockets/keppler_publicity/app/models/keppler_publicity/ad.rb
+++ rockets/keppler_publicity/app/models/keppler_publicity/ad.rb
@@ -12,7 +12,23 @@ module KepplerPublicity
     mount_uploader :image, AttachmentUploader
     acts_as_list
     acts_as_paranoid
+    validate  :check_dimensions, :on => [:create, :update]
+    validate  :validate_url
     validates_presence_of :image, :type_ad, :location, :url
+
+    def validate_url
+      url = URI.parse(self.url)
+        req = Net::HTTP.new(url.host, url.port)
+        req.use_ssl = true
+        if !url.host.nil?
+          res = req.request_head('/')
+          if !res.code.eql?('200')
+            errors.add(:url, "La URL introducida no es válida")
+          end
+        else
+          errors.add(:url, "La URL introducida no es válida")
+        end
+    end
 
     def self.have_actives(ad)
       where(
@@ -27,6 +43,23 @@ module KepplerPublicity
       return if actives.blank?
 
       actives.update(active: false)
+    end
+
+    def check_dimensions_type(width, height)
+      if (image.width < width) || ( image.height < height)
+          errors.add :image, "Dimensión incorrecta <br/> El tamaño de la imágen cargada es de: #{image.width}x#{image.height}px y no cumple con las dimensiones recomendadas: #{width}x#{height}".html_safe
+      end
+    end
+
+    def check_dimensions
+      return if image_cache.nil?
+      if type_ad.eql?('banner')
+        check_dimensions_type(1080, 700)
+      elsif type_ad.eql?('half-banner')
+        check_dimensions_type(900, 550)
+      elsif type_ad.eql?('steal-page')
+        check_dimensions_type(300, 550)
+      end
     end
 
     def self.index_attributes

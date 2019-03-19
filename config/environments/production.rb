02d83eec980ebec2b52a5397297a5b754dd56dc6
maria <castrogonzalezmariajose6@gmail.com>
2019-03-19 10:28:44 -0400

    Config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address:              'cloud.tremmelca.net',
-    port:                 2096,
+    port:                 587,
     domain:               '2xale.com',
     user_name:            'atencionalcliente@2xale.com',
     password:             'lbqQz1PVjg',

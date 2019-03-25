9651d861f6194fd888f7c869050ccb285b68ffa8
maria <castrogonzalezmariajose6@gmail.com>
2019-03-25 18:05:09 -0400

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -199,7 +199,7 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
 
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'cloud.tremmelca.com',
     port: 587,
     domain: '2xale.com',
     user_name: 'atencionalcliente@2xale.com',

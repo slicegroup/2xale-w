2ff2374f0f2c733f6dcdbaf586fbedb6ab3df7dc
maria <castrogonzalezmariajose6@gmail.com>
2019-03-25 18:14:13 -0400

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -192,7 +192,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.action_mailer.default_url_options =
-    { host: '2xale.com' }
+    { host: 'https://www.2xale.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
